fd25874c8712e9c7fcae6a2ced1c3518db3b674c
TaoZeyu <i@taozeyu.com>
2025-03-14 11:01:51 +0800

    feat(tasks): add pdf2md

---
+++ tasks/pdf-to-markdown/__init__.py
@@ -0,0 +1,45 @@
+#region generated meta
+import typing
+class Inputs(typing.TypedDict):
+  pdf: str
+  device: typing.Literal["cpu", "cuda"]
+  model_dir: typing.Optional[str]
+  output_file: typing.Optional[str]
+  assets_dir_name: str
+class Outputs(typing.TypedDict):
+  output_file: str
+#endregion
+
+import os
+
+from oocana import Context
+from tempfile import mkdtemp
+from pdf_craft import PDFPageExtractor, MarkDownWriter
+
+def main(params: Inputs, context: Context) -> Outputs:
+  pdf_path = params["pdf"]
+  model_dir = params["model_dir"]
+  output_file = params["output_file"]
+  assets_dir_name = params["assets_dir_name"]
+
+  if model_dir is None:
+    model_dir = mkdtemp()
+  if output_file is None:
+    output_file = os.path.join(
+      context.session_dir,
+      context.job_id,
+    )
+
+  extractor = PDFPageExtractor(
+    device=params["device"],
+    model_dir_path=model_dir,
+  )
+  with MarkDownWriter(
+    md_path=output_file,
+    assets_path=assets_dir_name,
+    encoding="utf-8",
+  ) as md:
+    for block in extractor.extract(pdf_path):
+      md.write(block)
+
+  return { "output_file": output_file }
