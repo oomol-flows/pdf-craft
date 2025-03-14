d3941792217f94b1b78fd7dce63e8df36c2f6551
TaoZeyu <i@taozeyu.com>
2025-03-14 15:24:10 +0800

    feat(tasks): support progress bar

--- tasks/pdf-to-markdown/__init__.py
+++ tasks/pdf-to-markdown/__init__.py
@@ -39,7 +39,10 @@ def main(params: Inputs, context: Context) -> Outputs:
     assets_path=assets_dir_name,
     encoding="utf-8",
   ) as md:
-    for block in extractor.extract(pdf_path):
+    for block in extractor.extract(
+      pdf=pdf_path,
+      report_progress=lambda i, n: context.report_progress(100.0 * i / n)
+    ):
       md.write(block)
 
   return { "output_file": output_file }
