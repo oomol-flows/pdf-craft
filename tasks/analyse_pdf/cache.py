2a6c974ea2c13874bd8320922929e645d0a6bfb2
Moskize91 <i@taozeyu.com>
2025-07-12 11:26:50 +0800

    feat(tasks): replace with new logic about cache files & update reporter logic (#47)
    * fix(tasks): reset weight for steps
    
    * refactor(tasks): extract reporter logic
    
    * feat(tasks): replace with new logic about cache files

--- tasks/analyse_pdf/cache.py
+++ tasks/analyse_pdf/cache.py
@@ -1,16 +1,60 @@
-import os
+import re
+import json
 import hashlib
+import shutil
 
+from typing import TypedDict
 from pathlib import Path
 from oocana import Context
 
+class _State(TypedDict):
+  pdf_path: str
+  pdf_mtime: float
+  pdf_hash: str
 
-def get_analysing_dir(context: Context, pdf_path: Path) -> Path:
-  cache_path = Path(context.tmp_pkg_dir) / context.node_id
-  pdf_hash = _calculate_sha512(pdf_path)
-  return cache_path / pdf_hash
+def get_or_prepare_cache_dir(context: Context, pdf_path: Path):
+  cache_path = Path(context.tmp_pkg_dir) / "pdf-craft"
+  json_path = cache_path / "state.json"
+  state: _State | None = None
+  if json_path.exists():
+    with open(json_path , "r", encoding="utf-8") as file:
+      state = json.loads(file.read())
 
-def _calculate_sha512(file_path):
+  should_write_state = False
+  should_clean_cache = False
+
+  pdf_path_str = re.sub(r"/$", "", str(pdf_path))
+  pdf_mtime = pdf_path.stat().st_mtime
+
+  if state is None:
+    should_write_state = True
+    should_clean_cache = True
+    state = {
+      "pdf_path": pdf_path_str,
+      "pdf_mtime": pdf_mtime,
+      "pdf_hash": _calculate_sha512(pdf_path),
+    }
+  else:
+    if pdf_path_str != state["pdf_path"] or pdf_mtime != state["pdf_mtime"]:
+      state["pdf_path"] = pdf_path_str
+      state["pdf_mtime"] = pdf_mtime
+      should_write_state = True
+      pdf_hash = _calculate_sha512(pdf_path)
+      if pdf_hash != state["pdf_hash"]:
+        state["pdf_hash"] = pdf_hash
+        should_clean_cache = True
+
+  if should_clean_cache:
+    shutil.rmtree(cache_path, ignore_errors=True)
+  cache_path.mkdir(parents=True, exist_ok=True)
+
+  if should_write_state:
+    with open(json_path, "w", encoding="utf-8") as file:
+      file.write(json.dumps(state, ensure_ascii=False))
+
+  return cache_path
+
+def _calculate_sha512(file_path: Path):
   sha512_hash = hashlib.sha512()
   chunk_size = 16384
   with open(file_path, "rb") as f:
